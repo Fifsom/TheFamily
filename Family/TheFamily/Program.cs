3fdaf39cc75119ad8981c291ca96e14845bb2d09
Fifsom <89588047+Fifsom@users.noreply.github.com>
2022-07-10 14:51:13 +0200

    dataseeding done

--- Family/TheFamily/Program.cs
+++ Family/TheFamily/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TheFamily.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<FamilyDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("FamilyContext")));
 
 var app = builder.Build();
 
@@ -14,6 +19,15 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using(var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<FamilyDbContext>();
+    context.Database.EnsureCreated();
+    DataSeeding.seedData(context);
 }
 
 app.UseHttpsRedirection();
